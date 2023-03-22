8a66aebb6480341750d69c79a126b286299318f7
barbardarbar <barbardarbar@outlook.com>
2023-03-22 15:03:34 +0100

    Added titles for the filters

--- src/dimsw-main-world.js
+++ src/dimsw-main-world.js
@@ -38,6 +38,10 @@ function addFilterCheckboxes() {
     g_Wishlist.rgValidFilters.push("mac_demo");
     g_Wishlist.rgValidFilters.push("linux_demo");
 
+    g_Wishlist.rgFilterPrefixMap.windows_demo = "Demo Availability"
+    g_Wishlist.rgFilterPrefixMap.mac_demo = "Demo Availability"
+    g_Wishlist.rgFilterPrefixMap.linux_demo = "Demo Availability"
+
     $J('#section_filters > .wrapper').append(demoPlatformsDiv);
 
     $J('.dimsw_filter').change(function () {
@@ -179,58 +183,3 @@ async function main() {
 }
 
 main();
-/*
-
-https://stackoverflow.com/questions/9915311/chrome-extension-code-vs-content-scripts-vs-injected-scripts
-
-*/
-// async function updateSearch() {
-//     await chrome.scripting
-//         .executeScript({
-//             target: { tabId: getTabId() },
-//             func: updategG_rgAppInfo,
-//             world: 'MAIN'
-//         })
-//         .then(() => console.log("injected a function"));
-// }
-
-// console.log("background");
-// // updateSearch();
-
-// async function getPageVar(name, tabId) {
-//     const [{ result }] = await chrome.scripting.executeScript({
-//         func: name => window[name],
-//         args: [name],
-//         target: {
-//             tabId: tabId ??
-//                 (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id
-//         },
-//         world: 'MAIN',
-//     });
-//     return result;
-// }
-
-
-// (async () => {
-//     const v = await getPageVar('g_rgAppInfo');
-//     console.log(v);
-// })();
-
-/*
-try sending message as in https://stackoverflow.com/questions/46869780/access-global-js-variables-from-js-injected-by-a-chrome-extension/46870005#46870005
-        > like this too https://stackoverflow.com/questions/9602022/chrome-extension-retrieving-global-variable-from-webpage
-better yet https://stackoverflow.com/questions/9515704/access-variables-and-functions-defined-in-page-context-using-a-content-script/9517879#9517879
-    > also try chrome.scripting.registerContentScripts with world: 'MAIN'
-
-*/
-
-// var g_alreadyRunOnce = false;
-// const WISHLIST_PAGE_REGEX = /store.steampowered.com\/wishlist\/([A-Za-z_0-9]+)(\/.*)?$/;
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (!tab.url?.match(WISHLIST_PAGE_REGEX)) return undefined;
-//     g_alreadyRunOnce = true;
-//     if (tab.active && changeInfo.status === "complete") {
-//         chrome.scripting.executeScript(...);
-//     }
-// })
